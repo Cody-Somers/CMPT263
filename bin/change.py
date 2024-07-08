b401c5fbea50b0c12355a04de1da4f2791ec5633
Somers <cas003@usask.ca>
2024-07-07 21:34:20 -0600

    Project Complete
    All docstrings and comments have been added. It is not capable of reading/writing to a text file as the given project example is simply in the command line.
    All test cases have been run and manually checked. No bugs found.

--- bin/change.py
+++ bin/change.py
@@ -1,10 +1,16 @@
 import math
 
+
 def main():
+    """
+    Asks user to input a number from the command line then prints out to the command line all the different permutations
+    that can be created of that number using coins of Canadian currency. Specifically 1,5,10,25,50,100, and 200 cent coins.
+
+    :return: Nothing
+    """
     # Check that the input from the user is a valid positive integer number.
     try:
-        #n = int(input("Enter a number: "))
-        n = 236 # Testing purposes. TODO: REMOVE THIS LINE
+        n = int(input("Enter a number: "))
     except:
         print("Invalid Input")
         return
@@ -12,15 +18,6 @@ def main():
         print("Invalid Input")
         return
 
-    # Set the denominations of the coins
-    c200 = 200
-    c100 = 100
-    c50 = 50
-    c25 = 25
-    c10 = 10
-    c5 = 5
-    c1 = 1
-    output = [0, 0, 0, 0, 0, 0, 0]  # Empty array set. First column is 200, second 100, etc.
     '''
     Pseudocode
     We get a value in. Then we find the largest denomination that fits.
@@ -30,9 +27,21 @@ def main():
     and repeat the process over again
     '''
 
+    # Set the denominations of the coins
+    c200 = 200  # 200 cent coin
+    c100 = 100
+    c50 = 50
+    c25 = 25
+    c10 = 10
+    c5 = 5
+    c1 = 1
+    output = [0, 0, 0, 0, 0, 0, 0]  # Empty array set. First column is # of 200 cent coins, second is 100, etc.
+    print("[200 cent coins, 100 cent coins, 50 cent coins, 25 cent coins, 10 cent coins, 5 cent coins, 1 cent coins]")
+    print("Example: [0,1,1,0,0,1,3] is equal to an input of 158\n")
+
     # Initialize the input array
     x = n/c200  # Check if greater that 200 cents
-    if x >= 1:
+    if x >= 1:  # If at least 1 coin fits
         x = math.floor(x)  # Round down
         output[0] = x  # Set first index of array to number of 200 cent coins
         n = n - x * c200  # Subtract the total amount by the number of 200 cent coins that fit
@@ -75,6 +84,11 @@ def main():
         if n != 0:
             print("Error with array initialization")
     print(output)  # First combination
+
+    # Check from the largest denomination for the first non-zero value
+    # From there it is possible to find the remaining permutations
+    # It is also possible to simply call twohundredCents() and the process will propagate. However it may be slightly
+    # more efficient to first check for the lowest denomination to avoid the recursive effects as much as possible
     if output[0] != 0:
         twohundredCents(output)
     elif output[1] != 0:
@@ -87,29 +101,51 @@ def main():
         tenCents(output)
     else:
         fiveCents(output)
-    print("Test")
     return
 
 
 def fiveCents(output):
+    """
+    Takes in an array and prints out the permutations that occur from subtracting the five cent coins and replacing
+    their value with one cent coins.
+    :param output: [int]
+        An array of coins in the format [200,100,50,25,10,5,1]
+    :return: Nothing
+    """
     for i in range(output[5]):
         output[5] = output[5] - 1  # Reduce the number of five cent coins
-        output[6] = output[6] + 5  # add five since 1+1+1+1+1
+        output[6] = output[6] + 5  # add five since 5 = 1+1+1+1+1
         print(output)
 
+
 def tenCents(output):
+    """
+    Takes in an array and prints out the permutations that occur from subtracting the ten cent coins and replacing
+    their value with five cent coins.
+    :param output: [int]
+        An array of coins in the format [200,100,50,25,10,5,1]
+    :return: Nothing
+    """
     temp = output.copy()  # Used to reset the array back
     fiveCents(output)
     output = temp.copy()  # Return the coins from their 1 cent current state
     for j in range(output[4]):
         output[4] = output[4] - 1  # Reduce ten cent coins
-        output[5] = output[5] + 2  # add two since 5+5
+        output[5] = output[5] + 2  # add two since 10 = 5+5
         print(output)
         temp = output.copy()  # Used to reset the array back
         fiveCents(output)
         output = temp.copy()  # Return the coins from their 1 cent current state
 
+
 def twentyfiveCents(output):
+    """
+    Takes in an array and prints out the permutations that occur from subtracting the twenty-five cent coins and replacing
+    their value with ten and five cent coins.
+    :param output: [int]
+        An array of coins in the format [200,100,50,25,10,5,1]
+    :return: Nothing
+    """
     temp = output.copy()  # Used to reset the array back
     tenCents(output)
     output = temp.copy()  # Return the coins from their 1 cent current state
@@ -122,7 +158,15 @@ def twentyfiveCents(output):
         tenCents(output)
         output = temp.copy()  # Return the coins from their 1 cent current state
 
+
 def fiftyCents(output):
+    """
+    Takes in an array and prints out the permutations that occur from subtracting the fifty cent coins and replacing
+    their value with twenty-five cent coins.
+    :param output: [int]
+        An array of coins in the format [200,100,50,25,10,5,1]
+    :return: Nothing
+    """
     temp = output.copy()  # Used to reset the array back
     twentyfiveCents(output)
     output = temp.copy()  # Return the coins from their 1 cent current state
@@ -134,7 +178,15 @@ def fiftyCents(output):
         twentyfiveCents(output)
         output = temp.copy()  # Return the coins from their 1 cent current state
 
+
 def hundredCents(output):
+    """
+    Takes in an array and prints out the permutations that occur from subtracting the one-hundred cent coins and replacing
+    their value with fifty cent coins.
+    :param output: [int]
+        An array of coins in the format [200,100,50,25,10,5,1]
+    :return: Nothing
+    """
     temp = output.copy()  # Used to reset the array back
     fiftyCents(output)
     output = temp.copy()  # Return the coins from their 1 cent current state
@@ -146,7 +198,15 @@ def hundredCents(output):
         fiftyCents(output)
         output = temp.copy()  # Return the coins from their 1 cent current state
 
+
 def twohundredCents(output):
+    """
+    Takes in an array and prints out the permutations that occur from subtracting the two-hundred cent coins and replacing
+    their value with one-hundred cent coins.
+    :param output: [int]
+        An array of coins in the format [200,100,50,25,10,5,1]
+    :return: Nothing
+    """
     temp = output.copy()  # Used to reset the array back
     hundredCents(output)
     output = temp.copy()  # Return the coins from their 1 cent current state
@@ -158,4 +218,7 @@ def twohundredCents(output):
         hundredCents(output)
         output = temp.copy()  # Return the coins from their 1 cent current state
 
+
+# Used to call the main program and run in the command line
 main()
+
